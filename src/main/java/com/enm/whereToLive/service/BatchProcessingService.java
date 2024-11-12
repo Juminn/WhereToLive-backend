9bddf0e169f31da031bbb3fa92606d63906a6d02
jumin <5216863@gmail.com>
2024-11-12 16:19:43 +0900

    refactoring logging

--- src/main/java/com/enm/whereToLive/service/BatchProcessingService.java
+++ src/main/java/com/enm/whereToLive/service/BatchProcessingService.java
@@ -15,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -42,6 +44,7 @@ public class BatchProcessingService {
     // 매일 자정에 실행
     //@Scheduled(cron = "0 0 0 * * *")
     //@PostConstruct
+    //@EventListener(ApplicationReadyEvent.class)
     public void processDailyClusters() throws Exception {
 
         while (true) {
